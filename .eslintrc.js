f92f2a69f1f59c70ff4240c1890388b788cdeb5f
Alex Burley <alexburley3@gmail.com>
2019-09-08 21:26:17 +0100

    Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": ["eslint:recommended", "plugin:prettier/recommended"],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-    "semi": ["error", "always"],
-    "quotes": ["error", "double"]
-    }
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    "no-console": "off",
+    semi: ["error", "always"],
+    quotes: ["error", "double"]
+  }
 };
--- scripts/generateFile.js
+++ scripts/generateFile.js
@@ -1,31 +1,31 @@
-const fs = require('fs');
+const fs = require("fs");
 function formatDate(date) {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+  var d = new Date(date),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
 
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
 
-    return [year, month, day].join('-');
+  return [year, month, day].join("-");
 }
 
 const postname = process.argv[2];
 if (!postname) {
-    console.log('Please supply a filename')
-    process.exit(1)
+  console.log("Please supply a filename");
+  process.exit(1);
 }
 
-const filename = `${formatDate(Date.now())}-${postname.split(' ').join('-')}.md`
-const filepath = `./posts/${filename}`
+const filename = `${formatDate(Date.now())}-${postname
+  .split(" ")
+  .join("-")}.md`;
+const filepath = `./posts/${filename}`;
 
-if (fs.existsSync(filepath)){
-    console.log('File exists')
-    process.exit(1)
+if (fs.existsSync(filepath)) {
+  console.log("File exists");
+  process.exit(1);
 }
-console.log(fs.existsSync(filepath))
-fs.writeFileSync(`./posts/${filename}`, '')
-console.log(`Created post ${filename}`)
+console.log(fs.existsSync(filepath));
+fs.writeFileSync(`./posts/${filename}`, "");
+console.log(`Created post ${filename}`);
